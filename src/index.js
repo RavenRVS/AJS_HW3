70b2bb15c404fcf2ca88f05eb5188230722735bb
Vitaly Remnev <remnevvs@mail.ru>
2023-05-03 15:04:06 +0300

    change name package

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 function info() {
     const m = new Map();
-    m.set('name', 'ajs');
-    m.set('version', '1.0.0');
+    m.set('name', 'ajs_hw3');
+    m.set('version', '1.0.1');
     return m;
   }
   
